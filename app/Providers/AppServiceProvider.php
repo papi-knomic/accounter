50324ecfe012be1f78ee0627a6c4641a9ba496ba
Samson Moses <knomic10@gmail.com>
2023-10-14 14:23:55 +0100

    More initial codes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+	    Schema::defaultStringLength(191);
+	    User::observe( UserObserver::class );
     }
 }
